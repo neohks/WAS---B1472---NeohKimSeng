42a1f27be1a99363a0d7df3c7e25f2bddb37a847
Neoh Kim Seng <neohks21@gmail.com>
2020-06-05 22:49:18 +0800

    + Add Venue Event model

---
+++ ThAmCo.Events/Data/VenueEvent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ThAmCo.Events.Data
+{
+    public class VenueEvent
+    {
+        [Key, MinLength(5), MaxLength(5)]
+        public string Code { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Range(1, Int32.MaxValue)]
+        public int Capacity { get; set; }
+    }
+}
